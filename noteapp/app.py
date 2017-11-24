932cf1539060ecfcf606f549860c101ba8892afc
m3zoid <vishboris@gmail.com>
2017-11-24 17:03:08 +0200

    Add 2-nd code part

--- noteapp/app.py
+++ noteapp/app.py
@@ -1,8 +1,7 @@
 from flask import Flask
+from noteapp.views.index import bp as index_bp
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello and HI!"
+app.register_blueprint(index_bp)
